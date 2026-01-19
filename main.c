06a87ec326798629ef82d3a19baab3a657c120f0
ricardocosmico <jeffthepeep80@gmail.com>
2026-01-19 18:08:59 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,18 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef ENDERECO_H
+#define ENDERECO_H
 #define MAX_VENDAS 10
-#include <string.h>
-#include <ctype.h>
-void Endereco(){
-    printf("executada");
-}
-
-int main()
-{
-    Endereco();
-    return 0;
-}
-
+//preencher dados do terreno
 typedef struct{
     char rua[20];
     int numero;
@@ -33,34 +22,12 @@ typedef struct{
     Lote lote_t;
 
 }Venda;
+//declaraçao das funcoes
+extern Venda vendas[MAX_VENDAS];
+extern int totalvendas;
+extern int proximoId;
 
-Venda vendas[MAX_VENDAS];
-int totalvendas = 0;
-int proximoId = 1;
+void limparbuffer();
+void cadastrarVenda();
 
-void limparbuffer(){
-   int c;
-   while ((c = getchar()) != '\n' && c != EOF);
-}
-
-void cadastrarVenda(){
-if (totalvendas >= MAX_VENDAS){
-    printf("atingiu limite de vendas ")
-
-}
-
-Venda novaVenda;
-
-novaVenda.ID = proximoId++;
-//comprador
-
-printf("dados comprador:\n");
-fgets(novaVenda.comprador, 100, stdin);
-novaVenda.comprador[strcspn(novaVenda.comprador, "\n")]=0;
-
-//coleta valor da venda
-printf("valor final da venda:\n");
-scanf("%f", &novaVenda.valor_final);
-limparbuffer();
-}
-}
+#endif
