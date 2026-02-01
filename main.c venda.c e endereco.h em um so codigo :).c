4b1396f8fdb53d224955580e359bc27314c91c1c
ricardocosmico <jeffthepeep80@gmail.com>
2026-02-01 18:01:55 -0300

    Update main.c venda.c e endereco.h em um so codigo :).c

--- main.c venda.c e endereco.h em um so codigo :).c
+++ main.c venda.c e endereco.h em um so codigo :).c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <locale.h>
+
 #define MAX_VENDAS 10
 #define ARQUIVO_DADOS "./vendas.txt"
 
@@ -105,7 +105,7 @@ int cadastrarVenda() {
     } while (!cepValido);
 
     vendas[totalvendas++] = v;
-    printf(GREEN "Venda cadastrada com sucesso na memoria!\n" RESET);
+    printf(GREEN "Venda cadastrada com sucesso!\n" RESET);
     return 1;
 }
 
@@ -114,11 +114,17 @@ void listarVendas() {
         printf(RED "\nNenhuma venda cadastrada.\n" RESET);
         return;
     }
-    printf("\nID | Comprador | Valor | Quadra | Dim. | CEP\n");
+    printf("\nID | Comprador | Valor | Quadra | Dim. | Rua | Num | CEP\n");
+    printf("-----------------------------------------------------------\n");
     for (int i = 0; i < totalvendas; i++) {
-        printf("%02d | %s | %.2f | %s | %.2f | %s\n",
-               vendas[i].ID, vendas[i].comprador, vendas[i].valor_final,
-               vendas[i].lote_t.quadra, vendas[i].lote_t.dimensao,
+        printf("%02d | %s | %.2f | %s | %.2f | %s | %d | %s\n",
+               vendas[i].ID, 
+               vendas[i].comprador, 
+               vendas[i].valor_final,
+               vendas[i].lote_t.quadra, 
+               vendas[i].lote_t.dimensao,
+               vendas[i].lote_t.endereco_t.rua,
+               vendas[i].lote_t.endereco_t.numero,
                vendas[i].lote_t.endereco_t.cep);
     }
 }
@@ -144,32 +150,35 @@ int excluirVenda(int id) {
     return 1;
 }
 
+// SALVA TODOS OS DADOS NO TXT
 int salvarVendasArquivo() {
     FILE *f = fopen(ARQUIVO_DADOS, "w");
     if (!f) {
-        printf(RED "Erro ao abrir o arquivo para escrita!\n" RESET);
+        printf(RED "Erro ao abrir o arquivo!\n" RESET);
         return 0;
     }
 
     fprintf(f, "PROXIMO_ID:%d\n", proximoId);
     for (int i = 0; i < totalvendas; i++) {
+        // Garantindo que todos os campos da struct sejam escritos
         fprintf(f, "%d|%s|%.2f|%s|%.2f|%s|%d|%s\n",
-                vendas[i].ID, vendas[i].comprador, vendas[i].valor_final,
-                vendas[i].lote_t.quadra, vendas[i].lote_t.dimensao,
-                vendas[i].lote_t.endereco_t.rua, vendas[i].lote_t.endereco_t.numero,
+                vendas[i].ID, 
+                vendas[i].comprador, 
+                vendas[i].valor_final,
+                vendas[i].lote_t.quadra, 
+                vendas[i].lote_t.dimensao,
+                vendas[i].lote_t.endereco_t.rua, 
+                vendas[i].lote_t.endereco_t.numero,
                 vendas[i].lote_t.endereco_t.cep);
     }
     fclose(f);
-    printf(GREEN "Dados salvos no arquivo com sucesso!\n" RESET);
+    printf(GREEN "Dados salvos com sucesso no arquivo TXT!\n" RESET);
     return 1;
 }
 
 int carregarVendasArquivo() {
     FILE *f = fopen(ARQUIVO_DADOS, "r");
-    if (!f) {
-        printf(RED "Arquivo nao encontrado para carregamento.\n" RESET);
-        return 0;
-    }
+    if (!f) return 0;
 
     totalvendas = 0;
     char linha[256];
@@ -187,11 +196,14 @@ int carregarVendasArquivo() {
         t = strtok(NULL, "|"); if (t) v.lote_t.dimensao = atof(t);
         t = strtok(NULL, "|"); if (t) strcpy(v.lote_t.endereco_t.rua, t);
         t = strtok(NULL, "|"); if (t) v.lote_t.endereco_t.numero = atoi(t);
-        t = strtok(NULL, "|"); if (t) strcpy(v.lote_t.endereco_t.cep, t);
+        t = strtok(NULL, "|"); if (t) {
+            // Remove o \n que sobra no final da string do CEP antes de copiar
+            t[strcspn(t, "\n")] = 0;
+            strcpy(v.lote_t.endereco_t.cep, t);
+        }
         vendas[totalvendas++] = v;
     }
     fclose(f);
-    printf(GREEN "Dados carregados com sucesso!\n" RESET);
     return 1;
 }
 
@@ -204,11 +216,11 @@ void mostrarConteudoArquivo() {
     printf("\n--- CONTEUDO BRUTO DO ARQUIVO ---\n");
     char c;
     while ((c = fgetc(f)) != EOF) printf("%c", c);
+    printf("---------------------------------\n");
     fclose(f);
 }
 
 int main() {
-    setlocale(LC_ALL, "Portuguese"); 
     int opcao, id_busca, pos;
     carregarVendasArquivo();
 
@@ -221,38 +233,30 @@ int main() {
         limparbuffer();
 
         switch (opcao) {
-            case 1:
-                cadastrarVenda();
-                break;
-            case 2:
-                listarVendas();
-                break;
+            case 1: cadastrarVenda(); break;
+            case 2: listarVendas(); break;
             case 3:
-                printf("Digite o ID para busca: ");
+                printf("ID para busca: ");
                 scanf("%d", &id_busca);
                 pos = buscarVendaPorID(id_busca);
-                if (pos != -1) printf(GREEN "ID %d encontrado: %s\n" RESET, id_busca, vendas[pos].comprador);
-                else printf(RED "Venda com ID %d nao encontrada.\n" RESET, id_busca);
+                if (pos != -1) {
+                    printf(GREEN "Venda Encontrada!\n" RESET);
+                    printf("Comprador: %s | Valor: %.2f | Rua: %s, %d\n", 
+                            vendas[pos].comprador, vendas[pos].valor_final, 
+                            vendas[pos].lote_t.endereco_t.rua, vendas[pos].lote_t.endereco_t.numero);
+                }
+                else printf(RED "Nao encontrado.\n" RESET);
                 break;
             case 4:
-                printf("Digite o ID para excluir: ");
+                printf("ID para excluir: ");
                 scanf("%d", &id_busca);
                 excluirVenda(id_busca);
                 break;
-            case 5:
-                salvarVendasArquivo();
-                break;
-            case 6:
-                carregarVendasArquivo();
-                break;
-            case 7:
-                mostrarConteudoArquivo();
-                break;
-            case 0:
-                printf("Saindo...\n");
-                break;
-            default:
-                printf(RED "Opcao invalida!\n" RESET);
+            case 5: salvarVendasArquivo(); break;
+            case 6: carregarVendasArquivo(); printf(GREEN "Dados recarregados!\n" RESET); break;
+            case 7: mostrarConteudoArquivo(); break;
+            case 0: printf("Saindo...\n"); break;
+            default: printf(RED "Invalido!\n" RESET);
         }
     } while (opcao != 0);
 
